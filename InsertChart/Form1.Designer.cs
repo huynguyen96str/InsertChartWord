b38041264b455b0cfc663904deee68d298c51e58
huynguyen96str <huynguyen96str@gmail.com>
2023-05-10 01:14:16 +0700

    Da xong phan cua a tuan

--- InsertChart/Form1.Designer.cs
+++ InsertChart/Form1.Designer.cs
@@ -29,24 +29,24 @@ namespace InsertChart
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pgb = new System.Windows.Forms.ProgressBar();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnBrowser = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnBrowser = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // progressBar1
+            // pgb
             // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.progressBar1.Location = new System.Drawing.Point(0, 25);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(570, 10);
-            this.progressBar1.TabIndex = 4;
+            this.pgb.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pgb.Location = new System.Drawing.Point(0, 31);
+            this.pgb.Name = "pgb";
+            this.pgb.Size = new System.Drawing.Size(384, 10);
+            this.pgb.TabIndex = 4;
             // 
             // panel1
             // 
@@ -55,28 +55,8 @@ namespace InsertChart
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 25);
+            this.panel1.Size = new System.Drawing.Size(384, 31);
             this.panel1.TabIndex = 5;
-            // 
-            // btnBrowser
-            // 
-            this.btnBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnBrowser.Location = new System.Drawing.Point(0, 0);
-            this.btnBrowser.Name = "btnBrowser";
-            this.btnBrowser.Size = new System.Drawing.Size(111, 25);
-            this.btnBrowser.TabIndex = 2;
-            this.btnBrowser.Text = "Browser";
-            this.btnBrowser.UseVisualStyleBackColor = true;
-            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.btnBrowser);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(459, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(111, 25);
-            this.panel2.TabIndex = 4;
             // 
             // panel3
             // 
@@ -84,7 +64,7 @@ namespace InsertChart
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(459, 25);
+            this.panel3.Size = new System.Drawing.Size(273, 31);
             this.panel3.TabIndex = 5;
             // 
             // textBox1
@@ -93,33 +73,53 @@ namespace InsertChart
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(459, 25);
+            this.textBox1.Size = new System.Drawing.Size(273, 31);
             this.textBox1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnBrowser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(273, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(111, 31);
+            this.panel2.TabIndex = 4;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBrowser.Location = new System.Drawing.Point(0, 0);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(111, 31);
+            this.btnBrowser.TabIndex = 2;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(570, 35);
+            this.ClientSize = new System.Drawing.Size(384, 41);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pgb);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(586, 74);
+            this.MaximumSize = new System.Drawing.Size(400, 80);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(586, 74);
+            this.MinimumSize = new System.Drawing.Size(400, 80);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Insert chart to word";
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar pgb;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.TextBox textBox1;
--- InsertChart/Form1.cs
+++ InsertChart/Form1.cs
@@ -11,6 +11,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using XlAxisGroup = Microsoft.Office.Interop.Word.XlAxisGroup;
 using XlAxisType = Microsoft.Office.Core.XlAxisType;
+using System.Diagnostics;
+using Points = Microsoft.Office.Core.Points;
 
 namespace InsertChart
 {
@@ -19,96 +21,142 @@ namespace InsertChart
         public Form1()
         {
             InitializeComponent();
+            pgb.Visible = false;
         }
-
-        private void btnBrowser_Click(object sender, EventArgs e)
+        Microsoft.Office.Interop.Word.Application wordApplication;
+        Microsoft.Office.Interop.Word.Document doc;
+        object oMissing;
+        private async void btnBrowser_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter  = @"Docx Files|*.docx;*.doc;";
-            if(openFileDialog.ShowDialog()== DialogResult.OK)
+            openFileDialog.Filter = @"Docx Files|*.docx;*.doc;";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                string fn = openFileDialog.FileName;
-
-
-                Microsoft.Office.Interop.Word.Application wordApplication = new Microsoft.Office.Interop.Word.Application();
-
-                object oMissing = System.Reflection.Missing.Value;
+                pgb.Visible = true;
+                btnBrowser.Enabled = false;
+                wordApplication = new Microsoft.Office.Interop.Word.Application();
+                oMissing = System.Reflection.Missing.Value;
                 Object oFalse = false;
-                Object filename = (Object)(fn);
+                Object filename = (Object)(openFileDialog.FileName);
 
-                Microsoft.Office.Interop.Word.Document doc = wordApplication.Documents.Open(ref filename, ref oMissing,
+                doc = wordApplication.Documents.Open(ref filename, ref oMissing,
                     ref oFalse, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
                     ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
                     ref oMissing, ref oMissing, ref oMissing, ref oMissing);
 
                 doc.Activate();
-                // code Region
-                // Bước 1: tạo table
-                Table dataTable = doc.Tables.Add(doc.Range(), 4, 3);
-                dataTable.Cell(1, 1).Range.Text = "Tháng";
-                dataTable.Cell(1, 2).Range.Text = "Doanh thu";
-                dataTable.Cell(1, 3).Range.Text = "Lợi nhuận";
-                dataTable.Cell(2, 1).Range.Text = "Tháng 1";
-                dataTable.Cell(2, 2).Range.Text = "100";
-                dataTable.Cell(2, 3).Range.Text = "20";
-                dataTable.Cell(3, 1).Range.Text = "Tháng 2";
-                dataTable.Cell(3, 2).Range.Text = "150";
-                dataTable.Cell(3, 3).Range.Text = "30";
-                dataTable.Cell(4, 1).Range.Text = "Tháng 3";
-                dataTable.Cell(4, 2).Range.Text = "200";
-                dataTable.Cell(4, 3).Range.Text = "40";
-                // https://blog.conholdate.com/total/create-charts-in-word-documents-using-csharp/
-                // Tạo một Chart
-                Range range = doc.Range(dataTable.Range.End, dataTable.Range.End);
-                Chart chart = doc.InlineShapes.AddChart(XlChartType.xlCombo, range).Chart;
 
-                
-                // Thêm các series vào biểu đồ
-                Series series1 = (Series)chart.SeriesCollection(1);
-                series1.Name = "Doanh thu";
-                series1.ChartType = XlChartType.xlColumnClustered;
-                series1.Values = new double[] { 2.7, 3.2, 0.8 }; // .value
-                series1.AxisGroup = XlAxisGroup.xlPrimary;
-
-                Series series2 = (Series)chart.SeriesCollection(2);
-                series2.Name = "Lợi nhuận";
-                series2.ChartType = XlChartType.xlLineMarkers;
-                series2.Values = new double[] { 1, 25, 5 }; //.value
-                series2.AxisGroup = XlAxisGroup.xlSecondary;
-
-                // Đổi tên trục X và trục Y
-                Axis xAxis = (Axis)chart.Axes(XlAxisType.xlCategory, XlAxisGroup.xlPrimary);
-                xAxis.HasTitle = true;
-                xAxis.AxisTitle.Text = "Tháng";
-
-                Axis yAxis1 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlPrimary);
-                yAxis1.HasTitle = true;
-                yAxis1.AxisTitle.Text = "Doanh thu";
-
-                Axis yAxis2 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlSecondary);
-                yAxis2.HasTitle = true;
-                yAxis2.AxisTitle.Text = "Lợi nhuận";
-
-                // Đổi màu và kiểu của series
-                //series1.Format.Fill.ForeColor.RGB = (int)XlRgbColor.rgbRed;
-                //series2.Format.Line.ForeColor.RGB = (int)XlRgbColor.rgbBlue;
-
-                var pathfilename = @"C:\Users\HUY NGUYEN\Desktop\export\thuyetminh.pdf";
-                Object filename2 = (Object)pathfilename;
-
-                doc.SaveAs(ref filename2, WdSaveFormat.wdFormatPDF,
-                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
-                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
-                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
-
-
-                // close word doc and word app.
-                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
-                ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
-                ((_Application)wordApplication).Quit(ref oMissing, ref oMissing, ref oMissing);
-                wordApplication = null;
-                doc = null;
+                try
+                {
+                    for (int i = 1; i < doc.Tables.Count; i++)
+                    {
+                        SetTableComb(doc.Tables[i]);
+                        pgb.Value = (int)(i * 100.0 / doc.Tables.Count);
+                    }
+                    MessageBox.Show("Finished!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    Release();
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                btnBrowser.Enabled = true;
             }
+            pgb.Visible = false;
+            await System.Threading.Tasks.Task.Delay(1);
+        }
+        private void Release()
+        {
+            //var pathfilename = @"C:\Users\PC\Desktop\Export\thuyetminh.pdf";
+            //Object filename2 = (Object)pathfilename;
+            //doc.SaveAs(ref filename2, WdSaveFormat.wdFormatPDF,
+            //    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+            //    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+            // close word doc and word app.
+            object saveChanges = WdSaveOptions.wdSaveChanges;
+            ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+            ((_Application)wordApplication).Quit(ref oMissing, ref oMissing, ref oMissing);
+            wordApplication = null;
+            doc = null;
+        }
+        private void SetTableComb(Table tb)
+        {
+            if (!(tb.Columns.Count >= 4)) return;
+            if (!(tb.Rows.Count > 1)) return;
+            int rowstartIndex = 2;
+            int rowsEndIndex = tb.Rows.Count - 1;
+            // create Excel application object
+            Range range = doc.Range(tb.Range.End, tb.Range.End);
+            //Chart chart = doc.InlineShapes.AddChart(XlChartType.xlCombo, range).Chart;
+            Chart chart = doc.InlineShapes.AddChart2(-1, XlChartType.xlCombo, range, Type.Missing).Chart;
+            string title = tb.Cell(1, tb.Columns.Count - 2).Range.Text.Trim().Replace("\r", "").Replace("\a", "");
+            chart.ChartTitle.Text = title;
+            chart.HasTitle = title.Length > 0;
+            int count = rowsEndIndex - rowstartIndex + 1;
+            double[] seri1 = new double[count];
+            double[] seri2 = new double[count];
+            string[] lb = new string[count];
+            int index = 0;
+            string tt = "";
+            for (int r = rowstartIndex; r <= rowsEndIndex; r++)
+            {
+                string v1 = tb.Cell(r, tb.Columns.Count - 1).Range.Text.Trim().Replace(",", ".").Replace("\r", "").Replace("\a", "");
+                string v2 = tb.Cell(r, tb.Columns.Count).Range.Text.Trim().Replace(",", ".").Replace("\r", "").Replace("\a", "");
+                seri1[index] = Convert.ToDouble(v1);
+                seri2[index] = Convert.ToDouble(v2);
+                tt = tb.Cell(r, tb.Columns.Count - 2).Range.Text.Trim().Replace("\r", "").Replace("\a", "");
+                if (tt.Length > 60) tt = tt.Substring(0, 30) + "...";
+                lb[index] = tt;
+                index++;
+            }
+            if (lb.Count() <= 4)
+            {
+                ChartGroup cg1 = (ChartGroup)chart.ChartGroups(1);
+                cg1.GapWidth = 500;
+            }
+            // Thêm các series vào biểu đồ
+            Series series1 = (Series)chart.SeriesCollection(1);
+            series1.Name = tb.Cell(1, tb.Columns.Count - 1).Range.Text;
+            series1.ChartType = XlChartType.xlColumnClustered;
+            series1.Values = seri1; // .value
+            series1.AxisGroup = XlAxisGroup.xlPrimary;
+            series1.ApplyDataLabels(Microsoft.Office.Interop.Word.XlDataLabelsType.xlDataLabelsShowValue);
+            DataLabels dlb1 = (DataLabels)series1.DataLabels();
+            series1.Format.Fill.ForeColor.RGB = (int)XlRgbColor.xlGreen;
+            dlb1.Font.Color = ColorTranslator.ToOle(Color.White);
+            dlb1.Position = Microsoft.Office.Interop.Word.XlDataLabelPosition.xlLabelPositionCenter;
+
+            Series series2 = (Series)chart.SeriesCollection(2);
+            series2.Name = tb.Cell(1, tb.Columns.Count).Range.Text;
+            series2.ChartType = XlChartType.xlLineMarkers;
+            series2.Values = seri2; //.value
+            series2.AxisGroup = XlAxisGroup.xlSecondary;
+            series2.ApplyDataLabels(Microsoft.Office.Interop.Word.XlDataLabelsType.xlDataLabelsShowValue);
+            DataLabels dlb2 = (DataLabels)series2.DataLabels();
+            series2.Format.Fill.ForeColor.RGB = (int)XlRgbColor.xlOrange;
+            dlb2.Font.Color = ColorTranslator.ToOle(Color.Orange);
+            dlb2.NumberFormat =  "General\\%"; ;
+            dlb2.Position = Microsoft.Office.Interop.Word.XlDataLabelPosition.xlLabelPositionAbove;
+            Series series3 = ((Series)chart.SeriesCollection(3));
+            series3.Delete();
+
+            chart.SeriesCollection(1).XValues = lb;
+
+            // Đổi tên trục X và trục Y
+            //Axis xAxis = (Axis)chart.Axes(XlAxisType.xlCategory, XlAxisGroup.xlPrimary);
+            //xAxis.HasTitle = true;
+            //xAxis.AxisTitle.Text = "Tháng";
+
+            Axis yAxis1 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlPrimary);
+            yAxis1.HasTitle = true;
+            yAxis1.AxisTitle.Text = tb.Cell(1, tb.Columns.Count - 1).Range.Text;
+
+            Axis yAxis2 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlSecondary);
+            yAxis2.HasTitle = true;
+            yAxis2.AxisTitle.Text = tb.Cell(1, tb.Columns.Count).Range.Text;
+            chart.ChartData.Workbook.close();
         }
     }
 }
