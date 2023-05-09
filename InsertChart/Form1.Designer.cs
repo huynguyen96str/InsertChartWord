bdd861f0017cad0bcc3cc7dfa9077bfe7ce2261f
Huy Nguyễn <dreamofbaby0507@gmail.com>
2023-05-09 17:36:25 +0700

    d

---
+++ InsertChart/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace InsertChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(570, 10);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(570, 25);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBrowser.Location = new System.Drawing.Point(0, 0);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(111, 25);
+            this.btnBrowser.TabIndex = 2;
+            this.btnBrowser.Text = "Browser";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnBrowser);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(459, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(111, 25);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(459, 25);
+            this.panel3.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 25);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 35);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(586, 74);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(586, 74);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnBrowser;
+    }
+}
+
---
+++ InsertChart/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XlAxisGroup = Microsoft.Office.Interop.Word.XlAxisGroup;
+using XlAxisType = Microsoft.Office.Core.XlAxisType;
+
+namespace InsertChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter  = @"Docx Files|*.docx;*.doc;";
+            if(openFileDialog.ShowDialog()== DialogResult.OK)
+            {
+                string fn = openFileDialog.FileName;
+
+
+                Microsoft.Office.Interop.Word.Application wordApplication = new Microsoft.Office.Interop.Word.Application();
+
+                object oMissing = System.Reflection.Missing.Value;
+                Object oFalse = false;
+                Object filename = (Object)(fn);
+
+                Microsoft.Office.Interop.Word.Document doc = wordApplication.Documents.Open(ref filename, ref oMissing,
+                    ref oFalse, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+                doc.Activate();
+                // code Region
+                // Bước 1: tạo table
+                Table dataTable = doc.Tables.Add(doc.Range(), 4, 3);
+                dataTable.Cell(1, 1).Range.Text = "Tháng";
+                dataTable.Cell(1, 2).Range.Text = "Doanh thu";
+                dataTable.Cell(1, 3).Range.Text = "Lợi nhuận";
+                dataTable.Cell(2, 1).Range.Text = "Tháng 1";
+                dataTable.Cell(2, 2).Range.Text = "100";
+                dataTable.Cell(2, 3).Range.Text = "20";
+                dataTable.Cell(3, 1).Range.Text = "Tháng 2";
+                dataTable.Cell(3, 2).Range.Text = "150";
+                dataTable.Cell(3, 3).Range.Text = "30";
+                dataTable.Cell(4, 1).Range.Text = "Tháng 3";
+                dataTable.Cell(4, 2).Range.Text = "200";
+                dataTable.Cell(4, 3).Range.Text = "40";
+                // https://blog.conholdate.com/total/create-charts-in-word-documents-using-csharp/
+                // Tạo một Chart
+                Range range = doc.Range(dataTable.Range.End, dataTable.Range.End);
+                Chart chart = doc.InlineShapes.AddChart(XlChartType.xlCombo, range).Chart;
+
+                
+                // Thêm các series vào biểu đồ
+                Series series1 = (Series)chart.SeriesCollection(1);
+                series1.Name = "Doanh thu";
+                series1.ChartType = XlChartType.xlColumnClustered;
+                series1.Values = new double[] { 2.7, 3.2, 0.8 }; // .value
+                series1.AxisGroup = XlAxisGroup.xlPrimary;
+
+                Series series2 = (Series)chart.SeriesCollection(2);
+                series2.Name = "Lợi nhuận";
+                series2.ChartType = XlChartType.xlLineMarkers;
+                series2.Values = new double[] { 1, 25, 5 }; //.value
+                series2.AxisGroup = XlAxisGroup.xlSecondary;
+
+                // Đổi tên trục X và trục Y
+                Axis xAxis = (Axis)chart.Axes(XlAxisType.xlCategory, XlAxisGroup.xlPrimary);
+                xAxis.HasTitle = true;
+                xAxis.AxisTitle.Text = "Tháng";
+
+                Axis yAxis1 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlPrimary);
+                yAxis1.HasTitle = true;
+                yAxis1.AxisTitle.Text = "Doanh thu";
+
+                Axis yAxis2 = (Axis)chart.Axes(XlAxisType.xlValue, XlAxisGroup.xlSecondary);
+                yAxis2.HasTitle = true;
+                yAxis2.AxisTitle.Text = "Lợi nhuận";
+
+                // Đổi màu và kiểu của series
+                //series1.Format.Fill.ForeColor.RGB = (int)XlRgbColor.rgbRed;
+                //series2.Format.Line.ForeColor.RGB = (int)XlRgbColor.rgbBlue;
+
+                var pathfilename = @"C:\Users\HUY NGUYEN\Desktop\export\thuyetminh.pdf";
+                Object filename2 = (Object)pathfilename;
+
+                doc.SaveAs(ref filename2, WdSaveFormat.wdFormatPDF,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+
+                // close word doc and word app.
+                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
+                ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+                ((_Application)wordApplication).Quit(ref oMissing, ref oMissing, ref oMissing);
+                wordApplication = null;
+                doc = null;
+            }
+        }
+    }
+}
